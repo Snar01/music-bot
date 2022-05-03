ec8e0eab3f3f51e4fa607d8d8e9552590571aad7
Snar <74876502+Snar01@users.noreply.github.com>
2022-05-03 22:28:55 +0100

    Update and rename config.js to exemple.config.js

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Guild: {
-        ID: "841340043712790538",
-        ServerDeafen: true,
-        DefaultVolume: 100
-    }
-}
---
+++ src/config/exemple.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    Guild: {
+        ID: "", //Your Guild ID
+        ServerDeafen: true, //If you want the bot as soon as it connects to the call, mute and hear nothing!
+        DefaultVolume: 100 //Server Default Volume
+    }
+}
