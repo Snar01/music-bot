255c8faa4df27e6a6e1755023476ddea5a80c878
Snar <74876502+Snar01@users.noreply.github.com>
2022-05-03 22:34:07 +0100

    Editing Lavalink variables
    Editing Lavalink variables

--- src/config/exemple.lavalink.js
+++ src/config/exemple.lavalink.js
@@ -1,10 +1,10 @@
 module.exports = {
     Lavalink_1: {
-        id: "NODE-01",
-        host: "",
-        password: "",
-        port: ,
-        secure: true
+        id: "NODE-01", //Name of the NODE, i like NODE-01, but you can rename
+        host: "", //The lavalink host, it can be, link, ip, etc...
+        password: "", //the Lavalink password
+        port: , //The Lavalink port
+        secure: true //If you use SSL place true, else place false
     }//, If you want add more NODE's, folow the exemples below, but don't forget, if you add more nodes, add the "," too, else will, give error
 
     /*Lavalink_2: {
