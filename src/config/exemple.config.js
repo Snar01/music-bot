7236a2d58d885e96e6e1d8073d4464e032c4be55
Snar <74876502+Snar01@users.noreply.github.com>
2022-05-04 11:45:47 +0100

    Delete src directory
    I'm deleting because this is the files of v1, and I not maked V2 of the Music BOT

--- src/config/exemple.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Guild: {
-        ID: "", //Your Guild ID
-        ServerDeafen: true, //If you want the bot as soon as it connects to the call, mute and hear nothing!
-        DefaultVolume: 100 //Server Default Volume
-    }
-}
--- src/structures/Event.js
+++
@@ -1,8 +0,0 @@
-class Event {
-    constructor(client, options) {
-        this.client = client;
-        this.name = options.name
-    }
-}
-
-module.exports = Event;
